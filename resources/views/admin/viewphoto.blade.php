cebaba98426a67644b214f9114eb44a33ce67fd4
baqar1 <baqarcit@gmail.com>
2017-04-21 07:51:34 +0500

    hello in viewhoto

--- resources/views/admin/viewphoto.blade.php
+++ resources/views/admin/viewphoto.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <hello></hello>
 
     <div class="row">
         <img src="{{ Storage::url($photo->image) }}" alt="" width="400px" height="400px">
